c9994cf8c5e1aa4b0f20c2a2a39aa4466c646577
alexknipfer <alexanderknipfer@gmail.com>
2018-04-16 15:28:13 -0500

    Reset password dialog

---
+++ dialogs/resetPassword.js
@@ -0,0 +1,39 @@
+const builder = require('botbuilder')
+
+const library = new builder.Library('resetPassword')
+
+library.dialog('/', [
+  session => {
+    session.beginDialog('validators:phonenumber', {
+      promptText: 'Please enter your phone number:',
+      retryPromptText:
+        'The value you entered is not a valid phone number, please try again.',
+      maxRetries: 2
+    })
+  },
+  (session, args) => {
+    session.dialogData.phoneNumber = args.response
+    session.send('The phone number you provided is: ' + args.response)
+
+    builder.Prompts.choice(session, 'Is this the correct phone number', [
+      'Yes',
+      'No'
+    ])
+  },
+  (session, results) => {
+    const { entity } = results.response
+
+    switch (entity) {
+      case 'Yes': {
+        session.send('Ok, great, lets continue with your phone number.')
+        break
+      }
+      case 'No': {
+        session.replaceDialog('resetPassword:/')
+        break
+      }
+    }
+  }
+])
+
+module.exports = library
