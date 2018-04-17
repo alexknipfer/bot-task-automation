7fd57f9090211ca1164c3d676069cfb083de363d
alexknipfer <alexanderknipfer@gmail.com>
2018-04-16 22:11:10 -0500

    Add email validator

--- validators.js
+++ validators.js
@@ -4,6 +4,10 @@ const PhoneRegex = new RegExp(
   /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/
 )
 
+const EmailRegex = new RegExp(
+  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/
+)
+
 const library = new builder.Library('validators')
 
 library.dialog(
@@ -15,6 +19,22 @@ library.dialog(
     })
     .matches(PhoneRegex, session => {
       console.log('MATCHES: ', session.message)
+      session.endDialogWithResult({ response: session.message.text })
+    })
+    .onDefault(session => {
+      session.send(session.dialogData.retryPromptText)
+    })
+)
+
+library.dialog(
+  'email',
+  new builder.IntentDialog()
+    .onBegin((session, args) => {
+      session.dialogData.retryPromptText = args.retryPromptText
+      session.send(args.promptText)
+    })
+    .matches(EmailRegex, session => {
+      console.log('EMAIL MATCHES!!!! ', session.message.text)
       session.endDialogWithResult({ response: session.message.text })
     })
     .onDefault(session => {
